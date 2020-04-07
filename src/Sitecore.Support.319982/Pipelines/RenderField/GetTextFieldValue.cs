934ec44c71a7ce7eab362e7e2f2faeaf82f138a7
kseniiayushina <kseniia.yushyna@sitecore.com>
2020-04-07 10:59:12 +0300

    Default code of GetTextFieldValue class

---
+++ src/Sitecore.Support.319982/Pipelines/RenderField/GetTextFieldValue.cs
@@ -0,0 +1,31 @@
+﻿namespace Sitecore.Support.Pipelines.RenderField
+{
+	using Sitecore.Configuration;
+	using Sitecore.Diagnostics;
+	using Sitecore.Pipelines.RenderField;
+	using System;
+	using System.Web;
+
+	public class GetTextFieldValue
+    {
+		public void Process(RenderFieldArgs args)
+		{
+			Assert.ArgumentNotNull(args, "args");
+			EncodeFieldValue(args);
+			string fieldTypeKey = args.FieldTypeKey;
+			if (fieldTypeKey.Equals("text", StringComparison.InvariantCulture) || fieldTypeKey.Equals("single-line text", StringComparison.InvariantCulture))
+			{
+				args.WebEditParameters.Add("prevent-line-break", "true");
+			}
+		}
+
+		protected virtual void EncodeFieldValue(RenderFieldArgs args)
+		{
+			Assert.ArgumentNotNull(args, "args");
+			if (Settings.Rendering.HtmlEncodedFieldTypes.Contains(args.FieldTypeKey))
+			{
+				args.Result.FirstPart = HttpUtility.HtmlEncode(args.Result.FirstPart);
+			}
+		}
+	}
+}
